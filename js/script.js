7011934d53161da381296fb0be35b0c23ae46d80
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-06-19 15:55:17 +0300

    added logic to the subscription form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+  $("form#registration").submit(function(event){
+    var name=$("input#name").val();
+    var email=$("input#email").val();
+    var plan=$("input#plan").val();
+
+    alert("thank you " + name + " for subscribing to " + plan + "plan.You will get further details in " + email);
+
+  });
+
+
+
+
+
+
+
   $("button#healthbutton").click(function(){
     $("#rowhealth").show();
   });
