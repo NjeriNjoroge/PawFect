0c7d1b7bb26a26e1d2e8d8cfbf5b581ce57df11f
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-06-20 12:38:20 +0300

    added prices for the products

--- js/script.js
+++ js/script.js
@@ -15,6 +15,36 @@ $(document).ready(function(){
     $("#rowhealth").hide();
     $("#feedrow").show();
   });
-
+  $(".pic1").click(function(){
+    $(".pic2").hide();
+    $(".pic3").hide();
+    $(".pic4").hide();
+    $(".originalformula").show();
+  })
+  $(".pic2").click(function(){
+    $(".pic1").hide();
+    $(".pic3").hide();
+    $(".pic4").hide();
+    $(".topdog").show();
+  })
+  $(".pic3").click(function(){
+    $(".pic2").hide();
+    $(".pic1").hide();
+    $(".pic4").hide();
+    $(".genetics").show();
+  })
+  $(".pic4").click(function(){
+    $(".pic2").hide();
+    $(".pic3").hide();
+    $(".pic1").hide();
+    $(".wildog").show();
+  })
+  $(".productinfo").click(function(){
+    $(".pic1").show();
+    $(".pic2").show();
+    $(".pic3").show();
+    $(".pic4").show();
+    $(".productinfo").hide();
+  })
 
 });
