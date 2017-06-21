1eb2f24e004e7b79d7d6f10781a9d01daf29cb30
Anita Nyamu <achieleyemo@gmail.com>
2017-06-21 08:59:18 +0300

    final commit

--- adopt/js/adopt.js
+++ adopt/js/adopt.js
@@ -1,30 +1,7 @@
-//var slideIndex = 0;
-//carousel();
+$(document).ready(function(){
+  $("button").click(function(){
+    $(".newform").show();
+  })
 
-//function carousel() {
-  //  var i;
-    //var x = document.getElementsByClassName("americanPitBull");
-    //for (i = 0; i < x.length; i++) {
-      //x[i].style.display = "none";
-    //}
-    //slideIndex++;
-    //if (slideIndex > x.length) {slideIndex = 1}
-    //x[slideIndex-1].style.display = "block";
-    //setTimeout(carousel, 5000); // Change image every 2 seconds
-//}
 
-//second carousel
-//var slideIndex = 0;/
-//ca/rousel();
-
-//function carousel() {
-  //  var i;
-  //  var x = document.getElementsByClassName("bichonfrise");
-    //for (i = 0; i < x.length; i++) {
-    //  x[i].style.display = "none";
-    //}
-    //slideIndex++;
-    //if (slideIndex > x.length) {slideIndex = 1}
-  //  x[slideIndex-1].style.display = "block";
-//    setTimeout(carousel, 5000); // Change image every 2 seconds
-//}
+});
